b2772ba8525bc7104c2ae776a4e583a0387cfd34
aaha <aaha.dev@gmail.com>
2026-09-21 22:24:38 +0530

    small fix

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -9,7 +9,7 @@ import { auth, googleProvider } from "../firebase";
 function Login() {
   // states for inputs
   const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [password, setPassword] = useState<string>("");
   const [usernameError, setUsernameError] = useState("");
   const [passwordError, setPasswordError] = useState("");
 
